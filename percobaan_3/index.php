138bc9251f90a47447f2d5cda29ee3e666e6b983
vypashaa <email-github-delvypashaa@gmail.com>
2026-09-23 20:18:48 +0700

    penambahan judul

--- percobaan_3/index.php
+++ percobaan_3/index.php
@@ -5,21 +5,22 @@
 //do ... while
 //foreach
 
+//PERULANGAN FOR
 // for($i = 0; $i < 5; $i++) {
 //     echo "Hello World! <br> ";
 // }
 
+// PERULANGAN WHILE
 // $i = 0;
 // while ($i < 5) {
 //     echo "Hello World! <br>";
 // $i++;
 // }
 
+//PERULANGAN DO ... WHILE
 // $i = 0;
 // do {
 //     echo "Hello World! <br>";
 //     $i++;
 // } while ($i < 5)
-
-
 ?>
