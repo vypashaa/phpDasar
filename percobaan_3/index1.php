e136aaa506fcaf29790810e8c75ec5f16c86950e
vypashaa <email-github-delvypashaa@gmail.com>
2026-09-23 20:01:15 +0700

    pengganti kurung kurawal

--- percobaan_3/index1.php
+++ percobaan_3/index1.php
@@ -7,13 +7,13 @@
 </head>
 <body>
     <table border = "1" cellpadding = "10" cellspacing = "0">
-       <?php for( $i = 1; $i <= 3; $i++ ) { ?>
+       <?php for( $i = 1; $i <= 3; $i++ ) : ?>
             <tr>
-                <?php for( $j = 1; $j <= 5; $j++ ){?>
+                <?php for( $j = 1; $j <= 5; $j++ ) : ?>
                 <td><?php echo "$i, $j";?></td>
-                <?php } ?>
+                <?php endfor; ?>
             </tr>
-       <?php } ?>
+       <?php endfor; ?>
     </table>
 </body>
 </html>
