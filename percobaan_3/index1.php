75ed3ed3b6c4717c2570b311b92853816f992d52
vypashaa <email-github-delvypashaa@gmail.com>
2026-09-23 19:57:16 +0700

    Memisahkan html dengan php

--- percobaan_3/index1.php
+++ percobaan_3/index1.php
@@ -7,14 +7,13 @@
 </head>
 <body>
     <table border = "1" cellpadding = "10" cellspacing = "0">
-        <?php
-            for($i = 1; $i <= 3; $i++) {
-                echo "<tr>";
-                for($j = 1; $j <= 5; $j++)
-                    echo "<td>$i, $j</td>";
-            } 
-                echo "</tr>";
-        ?>
+       <?php for( $i = 1; $i <= 3; $i++ ) { ?>
+            <tr>
+                <?php for( $j = 1; $j <= 5; $j++ ){?>
+                <td><?php echo "$i, $j";?></td>
+                <?php } ?>
+            </tr>
+       <?php } ?>
     </table>
 </body>
 </html>
