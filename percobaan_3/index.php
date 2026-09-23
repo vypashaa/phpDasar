5667d9d510542f84f7f6e33cfe4909134eccdec7
vypashaa <email-github-delvypashaa@gmail.com>
2026-09-23 18:18:25 +0700

    tambahan perulangan

---
+++ percobaan_3/index.php
@@ -0,0 +1,25 @@
+<?php 
+//pengulangan
+//for
+//while
+//do ... while
+//foreach
+
+// for($i = 0; $i < 5; $i++) {
+//     echo "Hello World! <br> ";
+// }
+
+// $i = 0;
+// while ($i < 5) {
+//     echo "Hello World! <br>";
+// $i++;
+// }
+
+// $i = 0;
+// do {
+//     echo "Hello World! <br>";
+//     $i++;
+// } while ($i < 5)
+
+
+?>
