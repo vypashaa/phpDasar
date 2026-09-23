720cf1cb9e42ae385b77fb8f259f53a7d7e1ce6b
vypashaa <email-github-delvypashaa@gmail.com>
2026-09-23 13:50:26 +0700

    first commit

---
+++ percobaan_2/index.php
@@ -0,0 +1,76 @@
+<?php 
+    //pertemuan 2
+    //syntax dasar
+
+
+    //ini komentar
+    //ini juga komentar
+    /* 
+        ini komentar
+        ini juga komnetar
+    */
+
+    //standar output
+    //echo, print
+    //print_r
+    //var_dump
+    // contoh = var_dump ("Delvyo PashaA");
+
+
+    //PENULISAN SYNTAX PHP:
+    //1. php di dalam html
+    //2. html di dalam php
+    //contoh nomor 2 = echo "<h1>Halo, namaku vyoo</h1>";
+
+    //variabel dan tipe data
+    //untuk variabel ($ + namaVar)
+    //$nama = "Delvyoo";
+    //echo "perkenalkan nama saya $nama";
+
+    //operator aritmatika
+    //(+ - * / %)
+    //echo 1 + 1
+    //$x = 60;
+    //$y = 60;
+    //echo $y + $x
+
+    //penghubung string / concatenation / concat
+    // .
+    //$nama_depan = "Delvyo";
+    //$nama_belakang = "Pasha";
+    //echo $nama_depan ." ".$nama_belakang;
+
+    //operator assignment
+    // =, +=, -=, *=, /=, %=, .=
+    //$p = 10;
+    //$p += 7;
+    //echo $p;
+    //$nama = "Delvyo";
+    //$nama .= " ";
+    //$nama .= "Pasha";
+    //echo "$nama";
+
+    //operator pembagian
+    //<, >, <=, >=, ==
+    //var_dump (1 == "1");
+
+    //operator identitas
+    //===, !==
+    //var_dump(1 !== "1");
+
+    //operator logika
+    //&&, ||, !
+    //$x = 11;
+    //var_dump($x < 30 || $x % 2 == 0);
+
+    
+    //echo "<h1>Hello $name</h1>";
+?>
+
+<?php
+    //$name = 'linus';
+    //echo "<h1>Hello $name</h1>";
+
+    echo "Ini adalah pesan";
+
+?>
