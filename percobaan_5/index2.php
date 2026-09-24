0dadce801041574c57a43ed22c040a551bf51f43
vypashaa <email-github-delvypashaa@gmail.com>
2026-09-24 21:18:17 +0700

    penampilan manual

---
+++ percobaan_5/index2.php
@@ -0,0 +1,23 @@
+<?php
+    $dataSiswa = ["Delvyo Pasha Adhityara", "0001", "RPL", "pasha01@gmail.com"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Siswa</title>
+</head>
+<body>
+    <h1>Daftar Nama Siswa</h1>
+
+    <ul>
+        <li>Delvyo Pasha Adhiyara</li>
+        <li>0001</li>
+        <li>RPL</li>
+        <li>pasha01@gmail.com</li>
+    </ul>
+</body>
+</html>
