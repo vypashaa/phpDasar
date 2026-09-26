a6482e9a0281851c318e737fa1741e4576ddeb90
vypashaa <email-github-delvypashaa@gmail.com>
2026-09-26 17:19:59 +0700

    coba echo variabel

--- percobaan_2/index1.php
+++ percobaan_2/index1.php
@@ -8,12 +8,24 @@
 <body>
     <?php 
         //echo "Percobaan file ke 2";
-
         $nama_depan = "Delvyo";
         $nama_tengah = "Pasha";
         $nama_belakang = "Adhityara";
 
-        echo "namaku " . $nama_depan . " " . $nama_tengah . " " . $nama_belakang;
+        echo "namaku $nama_depan $nama_tengah $nama_belakang";
+        echo "<br><hr>";
+
+        //echo variabel
+        $p = 20;
+        $l = 5;
+        $tambah = $p + $l;
+        $kurang = $p = $l;
+        $kali = $p * $l;
+        $bagi = $p / $l;
+        $sisaBagi = $p % $l;
+
+        echo "Penjumlahan: $tambah <br> Pengurangan: $kurang <br> Perkalian: $kali <br> Pembagiaan: $bagi <br> Sisa Bagi: $sisaBagi";
+
 
 
     ?>
